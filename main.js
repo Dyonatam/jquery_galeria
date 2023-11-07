8125ef3e2793813880ff22d9190937b4e88a73e1
Dyonatam Felix <dyonatamfelix@gmail.com>
2023-11-07 13:51:37 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+    $('header button').click(function() {
+        $('form').slideDown(350)
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp(200)
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault()
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val()
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoDaNovaImagem}" />`). appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `). appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn()
+        $('#endereco-imagem-nova').val('')
+    })
+
+
+
+})
+
+
